7c13907e81129f8e626546a951ce55f1984b2e50
JosueRomero777 <166617930+JosueRomero777@users.noreply.github.com>
2024-04-22 22:22:13 -0500

    Add files via upload

---
+++ Describe un auto con JS/js/javascript.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", home);
+
+function home() {
+    
+    const brand = "ford";
+    const model = "f-150";
+    const color = "rojo";
+    const plate = "AAC-0123";
+    const engine = 3.5;
+    const chassis_number = "1FMZU73EODZA69101";
+    const release_year = 2023;
+
+    alert("La marca de la camioneta es " + brand + " modelo " + model + " de color " + color + " con placa " + plate + " posee un motor " + engine + " con numero de chasis " + chassis_number + " y año de lanzamiento " + release_year);
+  }
