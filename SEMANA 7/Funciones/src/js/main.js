0998a9ac169f0925b20e2849777790f1ce997c71
JosueRomero777 <166617930+JosueRomero777@users.noreply.github.com>
2024-05-16 07:00:16 -0500

    Add files via upload

---
+++ SEMANA 7/Funciones/src/js/main.js
@@ -0,0 +1,68 @@
+'use strict';
+
+ document.addEventListener('DOMContentLoaded',function(){
+
+   fn1();
+   fn2();
+ });
+
+ 
+ 
+function fn1() {
+      
+
+  let pin;
+  let key = 3;
+  
+
+do {
+pin = prompt("Ingresa tu clave ", )
+
+
+} while (pin != key );
+alert("Clave correcta")
+}
+
+
+
+function fn2(i) {
+
+let pin;
+i = 3;
+
+pin = prompt("Ingresa tu clave ", )
+
+while (pin == i) {
+  alert("tu clave es correcta" );
+  break;
+}
+
+
+}
+
+
+
+
+function fn3() {
+      
+
+ return "Hello world"
+}
+
+
+alert(fn3());
+
+
+
+
+    
+function fn4(a) {
+      
+
+ return false;
+}
+
+
+alert(fn4());
+    
+
